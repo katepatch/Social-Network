3f7f819bc247af18f4501c68434fd7be1d977e3a
kate patch <kate.epatch@gmail.com>
2022-09-22 16:07:03 -0600

    created create user api

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,4 +19,14 @@ module.exports = {
                     )
             .catch((err) => res.status(500).son(err));
     },
+
+    createUser(req, res) {
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    },
+
 }
